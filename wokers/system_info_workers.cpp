fef3dd87c48c73392ee4c707523322d29cd15fe6
gis1501 <gis1501@gmail.com>
2019-11-13 21:21:07 +0300

    add system info

--- wokers/system_info_workers.cpp
+++ wokers/system_info_workers.cpp
@@ -15,11 +15,20 @@ void system_info_workers::slot_start_workers()
     font_info.set_parameter_name(tr("font family"));
     font_info.set_parameter_value(font.family());
 
+    // font family
     emit signal_result_system_info(font_info);
 
-    system_info cpu_info;
-    cpu_info.set_parameter_name(tr("cpu architecture"));
-    cpu_info.set_parameter_value(QSysInfo::currentCpuArchitecture());
+    // cpu architecture
+    emit signal_result_system_info(system_info(tr("cpu architecture"), QSysInfo::currentCpuArchitecture()));
+    // build cpu architecture
+    emit signal_result_system_info(system_info(tr("build cpu architecture"), QSysInfo::buildCpuArchitecture()));
+    // build abi
+    emit signal_result_system_info(system_info(tr("build abi"), QSysInfo::buildAbi()));
 
-    emit signal_result_system_info(cpu_info);
+    // kernel type
+    emit signal_result_system_info(system_info(tr("kernel type"), QSysInfo::kernelType()));
+    // kernel version
+    emit signal_result_system_info(system_info(tr("kernel version"), QSysInfo::kernelVersion()));
+    // product version
+    emit signal_result_system_info(system_info(tr("product version"), QSysInfo::productVersion()));
 }
