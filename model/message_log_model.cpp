b3cdee3678ad260dd19a71d2b36ea7207ce486d3
gis1501 <gis1501@gmail.com>
2019-11-15 17:37:09 +0300

    update class - message_log_model

--- model/message_log_model.cpp
+++ model/message_log_model.cpp
@@ -1,6 +1,101 @@
 #include "message_log_model.h"
 
-message_log_model::message_log_model()
+message_log_model::message_log_model(QObject *parent): QAbstractTableModel(parent)
 {
+    m_columns_map.insert(MC_LOG_ID, tr("наименование"));
+    m_columns_map.insert(MC_LOG_TEXT, tr("значение"));
+}
 
+message_log_model::~message_log_model()
+{
+}
+
+int message_log_model::columnCount(const QModelIndex &parent) const
+{
+    if (!parent.isValid())
+        return m_columns_map.size();
+    else
+        return 0;
+}
+
+QVariant message_log_model::data(const QModelIndex &index, int role) const
+{
+    if (index.isValid()) {
+        if ((index.row() >= 0) && (index.row() < m_data.size())) {
+            const auto &param = m_data.at(index.row());
+            switch(role){
+            case Qt::DisplayRole: {
+                switch (index.column()) {
+                case MC_LOG_ID:
+                    return param.message_log_id();
+                case DATAROLE_LOG_TEXT:
+                    return param.message_log_text();
+                default:
+                    break;
+                }
+                break;
+            }
+            case DATAROLE_LOG_ID:
+                return param.message_log_id();
+            case DATAROLE_LOG_TEXT:
+                return  param.message_log_text();
+            default:
+                Q_ASSERT("Не установлен обработчик возвращаемого результата");
+                break;
+            }
+        }
+    }
+    return QVariant();
+}
+
+QVariant message_log_model::headerData(int section, Qt::Orientation orientation, int role) const
+{
+    if ((role != Qt::DisplayRole) && (role != Qt::ToolTipRole))
+        return QVariant();
+    if ((section < 0) || (section >= m_columns_map.size()))
+        return QVariant();
+    return (orientation == Qt::Horizontal) ? QVariant::fromValue(m_columns_map.value(section))
+                                           : QVariant::fromValue(section + 1);
+}
+
+QHash<int, QByteArray> message_log_model::roleNames() const
+{
+    static QHash<int, QByteArray> roles {
+        {DATAROLE_LOG_ID, "message_log_id"},
+        {DATAROLE_LOG_TEXT, "message_log_text"}
+    };
+    return roles;
+}
+
+int message_log_model::rowCount(const QModelIndex &parent) const
+{
+    if (parent.isValid())
+        return 0;
+
+    return m_data.size();
+}
+
+bool message_log_model::removeRows(int row, int count, const QModelIndex &parent)
+{
+    beginRemoveRows(parent, row, row + count - 1);
+    m_data.remove(row, count);
+    endRemoveRows();
+
+    return true;
+}
+
+void message_log_model::add_data_to_model(const message_log &value)
+{
+    beginInsertRows(QModelIndex(), m_data.size(), m_data.size());
+    m_data.append(value);
+    endInsertRows();
+}
+
+void message_log_model::remove_data_from_model()
+{
+    if(m_data.size()>0) {
+        beginRemoveRows(QModelIndex(), 0, m_data.size() - 1);
+        m_data.clear();
+        endRemoveRows();
+    }
 }
--- model/message_log_model.h
+++ model/message_log_model.h
@@ -1,11 +1,39 @@
 #ifndef MESSAGE_LOG_MODEL_H
 #define MESSAGE_LOG_MODEL_H
 
+#include <QAbstractTableModel>
+#include "message_log.h"
 
-class message_log_model
+class message_log_model : public QAbstractTableModel
 {
+    Q_OBJECT
 public:
-    message_log_model();
+    enum data_role {
+        DATAROLE_LOG_ID = Qt::UserRole + 1,
+        DATAROLE_LOG_TEXT
+    };
+
+    enum model_columns {
+        MC_LOG_ID,
+        MC_LOG_TEXT
+    };
+
+    explicit message_log_model(QObject *parent = Q_NULLPTR);
+    ~message_log_model() Q_DECL_OVERRIDE;
+
+    int columnCount(const QModelIndex &parent) const Q_DECL_OVERRIDE;
+    QVariant data(const QModelIndex &index, int role) const Q_DECL_OVERRIDE;
+    QVariant headerData(int section, Qt::Orientation orientation, int role) const Q_DECL_OVERRIDE;
+    QHash<int, QByteArray> roleNames() const Q_DECL_OVERRIDE;
+    int rowCount(const QModelIndex &parent) const Q_DECL_OVERRIDE;
+    bool removeRows(int row, int count, const QModelIndex &parent = QModelIndex()) Q_DECL_OVERRIDE;
+
+    void add_data_to_model(const message_log &value);
+    void remove_data_from_model();
+
+private:
+    QMap<int, QString> m_columns_map;
+    QVector<message_log> m_data;
 };
 
 #endif // MESSAGE_LOG_MODEL_H
