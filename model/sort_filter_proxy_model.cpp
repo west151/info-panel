13875056eb2cc6d13034dc9317a241e91ac7195e
gis1501 <gis1501@gmail.com>
2019-11-15 17:23:51 +0300

    update class - sort_filter_proxy_model

--- model/sort_filter_proxy_model.cpp
+++ model/sort_filter_proxy_model.cpp
@@ -1,6 +1,20 @@
 #include "sort_filter_proxy_model.h"
 
-sort_filter_proxy_model::sort_filter_proxy_model()
+sort_filter_proxy_model::sort_filter_proxy_model(QObject *parent):
+    QSortFilterProxyModel(parent)
 {
 
+}
+
+bool sort_filter_proxy_model::filterAcceptsRow(int sourceRow, const QModelIndex &sourceParent) const
+{
+    if (filterRegExp().isEmpty())
+        return true;
+
+    for (int i = 0; i < sourceModel()->columnCount(); ++i) {
+        const auto index = sourceModel()->index(sourceRow, i, sourceParent);
+        if (sourceModel()->data(index).toString().toLower().trimmed().contains(filterRegExp()))
+            return true;
+    }
+    return false;
 }
--- model/sort_filter_proxy_model.h
+++ model/sort_filter_proxy_model.h
@@ -1,11 +1,16 @@
 #ifndef SORT_FILTER_PROXY_MODEL_H
 #define SORT_FILTER_PROXY_MODEL_H
 
+#include <QSortFilterProxyModel>
 
-class sort_filter_proxy_model
+class sort_filter_proxy_model : public QSortFilterProxyModel
 {
+    Q_OBJECT
 public:
-    sort_filter_proxy_model();
+    sort_filter_proxy_model(QObject *parent = nullptr);
+
+protected:
+    bool filterAcceptsRow(int sourceRow, const QModelIndex &sourceParent) const;
 };
 
 #endif // SORT_FILTER_PROXY_MODEL_H
